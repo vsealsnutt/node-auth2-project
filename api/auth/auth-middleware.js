a5e0f2a270f0f310c163524de53dcb462f87f667
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-03-20 10:30:06 -0400

    wrote the only middleware function

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -33,6 +33,11 @@ const restricted = (req, res, next) => {
 }
 
 const only = role_name => (req, res, next) => {
+  if (role_name === req.decodedjwt.role_name) {
+    next();
+  } else {
+    next({ status: 403, message: 'This is not for you' });
+  }
   /*
     If the user does not provide a token in the Authorization header with a role_name
     inside its payload matching the role_name passed to this function as its argument:
