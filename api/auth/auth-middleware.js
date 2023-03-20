6cc3f4a0d14b36230ff89cf6ca7ad75f4d6f6cb7
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-03-20 10:48:41 -0400

    wrote validateRoleName middleware

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -74,6 +74,16 @@ const checkUsernameExists = async (req, res, next) => {
 
 
 const validateRoleName = (req, res, next) => {
+  if (!req.body.role_name || !req.body.role_name.trim()) {
+    req.role_name = 'student';
+    next();
+  } else if (req.body.role_name.trim() === 'admin') {
+    next({ status: 422, message: 'Role name can not be admin' });
+  } else if (req.body.role_name.trim().length > 32) {
+    next({  status: 422, message: 'Role name can not be longer than 32 chars'});
+  } else {
+    next();
+  }
   /*
     If the role_name in the body is valid, set req.role_name to be the trimmed string and proceed.
 
