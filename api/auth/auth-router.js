54c642bc7b2e786796ed1ca0fc2f075029366cdd
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-03-20 11:06:49 -0400

    wrote register endpoint

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,8 +1,19 @@
 const router = require("express").Router();
 const { checkUsernameExists, validateRoleName } = require('./auth-middleware');
 const { JWT_SECRET } = require("../secrets"); // use this secret!
+const bcrypt = require('bcryptjs');
+const User = require('../users/users-model');
 
 router.post("/register", validateRoleName, (req, res, next) => {
+  const { username, password } = req.body;
+  const { role_name } = req;
+  const hash = bcrypt.hashSync(password, 8);
+
+  User.add({ username, password: hash, role_name })
+    .then(newUser => {
+      res.status(201).json(newUser);
+    })
+    .catch(next);
   /**
     [POST] /api/auth/register { "username": "anna", "password": "1234", "role_name": "angel" }
 
