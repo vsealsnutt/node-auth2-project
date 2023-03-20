9446a864b6ef5d7b87c85dc1b34c0eccf00af074
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-03-20 10:19:43 -0400

    wrote restricted middleware fuction

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,6 +1,20 @@
+const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = require("../secrets"); // use this secret!
 
 const restricted = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (token) {
+    jwt.verify(token, JWT_SECRET, (err, decoded) => {
+      if (err) {
+        next({ status: 401, message: 'Token invalid' });
+      } else {
+        req.decodedjwt = decoded;
+        next();
+      }
+    })
+  } else {
+    next({ status: 401, message: 'Token required' });
+  }
   /*
     If the user does not provide a token in the Authorization header:
     status 401
