f30077ff9de02a21c6fbceda1fc658329c135c44
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-03-20 19:46:56 -0400

    fixed only middleware function

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -33,7 +33,7 @@ const restricted = (req, res, next) => {
 }
 
 const only = role_name => (req, res, next) => {
-  if (role_name === req.decodedjwt.role_name) {
+  if (role_name === req.decoded.role_name) {
     next();
   } else {
     next({ status: 403, message: 'This is not for you' });
